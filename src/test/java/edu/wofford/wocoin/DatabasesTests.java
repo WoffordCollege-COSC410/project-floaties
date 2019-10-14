53161b36ebdc348f6e5a29274dee8763913ac730
Sergio Sum <summunozsa@email.wofford.edu>
2019-10-14 13:17:39 -0400

    Databases and Feature01Main with method ideas

---
+++ src/test/java/edu/wofford/wocoin/DatabasesTests.java
@@ -0,0 +1,16 @@
+/**
+ * Tests for class Databases.
+ *
+ * All tests in the folder "test" are executed
+ * when the "Test" action is invoked.
+ *
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatabasesTests{
+
+}
