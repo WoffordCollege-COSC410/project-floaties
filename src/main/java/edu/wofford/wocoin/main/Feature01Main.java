e167e8fc1590c42975c1599c9b3b3c088d6c171f
Kara <keporter3@gmail.com>
2019-10-15 12:12:01 -0400

    isAdmin made and tests made

--- src/main/java/edu/wofford/wocoin/main/Feature01Main.java
+++ src/main/java/edu/wofford/wocoin/main/Feature01Main.java
@@ -13,40 +13,44 @@ public class Feature01Main {
         Scanner scan = new Scanner(System.in);
 
         //prompt for 1st menu
+        System.out.println("1: exit" + '\n' +
+                "2: administrator" + '\n' +
+                "Choose one: ");
+        String selection1 = scan.nextLine();
 
+        while (!selection1.equals("1") && !selection1.equals("2")) {
+            System.out.println("Invalid input, enter 1 or 2: ");
+            selection1 = scan.nextLine();
+        }
 
-        System.out.println("Enter password: ");
-        String password = scan.nextLine();
+        if (selection1.equals("1")) {
+            System.exit(0);
+        } else {
+            System.out.println("Enter password: ");
+            String password = scan.nextLine();
 
-        if (d.checkIsAdmin(password)) {
-            System.out.println("1: back" + '\n' +
-                    "2: add user" + '\n' +
-                    "Choose one:  ");
-            String selection2 = scan.nextLine();
+            if (d.checkIsAdmin(password)) {
+                System.out.println("1: back" + '\n' +
+                        "2: add user" + '\n' +
+                        "Choose one:  ");
+                String selection2 = scan.nextLine();
 
-            while (!selection2.equals("1") && !selection2.equals("2")) {
-                System.out.println("Invalid input, enter 1 or 2: ");
-                selection2 = scan.nextLine();
+                while (!selection2.equals("1") && !selection2.equals("2")) {
+                    System.out.println("Invalid input, enter 1 or 2: ");
+                    selection2 = scan.nextLine();
+                }
+
+                if (selection2.equals("1")) {
+                    //back to menu 1
+                    System.out.println("You chose 1");
+                } else {
+                    //add user
+                    System.out.println("Add user");
+                }
             }
-
-            if (selection2.equals("1")) {
-                //back to menu 1
-                System.out.println("You chose 1");
-            } else {
-                //add user
-                System.out.println("Add user");
-            }
-
-
 
         }
     }
 }
 //=======
-// instantiate database object
-
-// scan for menu 1
-
-
-
 //>>>>>>> origin/master
